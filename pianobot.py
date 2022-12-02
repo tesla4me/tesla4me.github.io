360220448c8370ffb6f7b54540084f52361d0ebd
tesla4me <101115630+tesla4me@users.noreply.github.com>
2022-12-02 16:46:12 +0100

    Add files via upload

---
+++ pianobot.py
@@ -0,0 +1,36 @@
+# Hier werden die Bibliotheken importiert
+from pyautogui import *
+import pyautogui
+import time
+import keyboard
+import random
+import win32api, win32con, win32gui
+from pyWinActivate import win_activate, win_wait_active
+from time import sleep
+
+#Aktiviert das Fenster, anhand des Titel
+win_activate(window_title="Edge", partial_match=True)
+sleep(0.5)
+hwnd = win32gui.GetForegroundWindow()
+win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
+
+# Die nachfolgenden Zeilen definieren was beim Aufruf der Funktion "Click" passiert
+# Der Cursor geht auch die mitgegbene x-y Position und machen einen Mausklick
+def click(x,y): 
+      win32api.SetCursorPos((x,y)) 
+      win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0) 
+      time.sleep(0.03) #mimipause zwischen dem drücken und loslassen 
+      win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0) 
+      
+# Hier werden die viel Spalten nach den schwarzen Feldern geprüft
+# sobald schwarz detektiert wurde wird die Maus an dieser Position geklickt
+while keyboard.is_pressed ("q") == False: #wiederhole solange q nicht gedrückt ist 
+    if pyautogui.pixel (430, 625) [2] == 0:   # [0]rd [1]gn [2]bl -> weil schwarz -> nur ein Wert  auf 0 prüfen 
+        click (430, 615)  # entsprechende MausFunktion mit  X Y aufrufen 
+    if pyautogui.pixel (615, 625) [2] == 0:    #dito wie oben für Spalte 2
+        click (615, 615) 
+    if pyautogui.pixel (700, 625) [2] == 0:    #dito wie oben für Spalte 3
+        click (700, 615) 
+    if pyautogui.pixel (840, 625) [2] == 0:    #dito wie oben für Spalte 4
+        click (840, 615) 
+    
